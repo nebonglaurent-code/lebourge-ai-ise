749a3d685233a420b90f18bb4f0b923485fa9551
laurentNebong-code <nebongbourgeois@gmail.com>
2026-04-26 05:33:33 +0200

    Vercel config

--- api/index.py
+++ api/index.py
@@ -2,8 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from groq import Groq
-from dotenv import load_dotenv
-load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app)
